64fa8f3098b09ca0e9c81484128115d1ae07af3b
Jisi Guo <jisiguo@gmail.com>
2012-06-08 19:44:55 -0700

    finish user singup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
   
 end
