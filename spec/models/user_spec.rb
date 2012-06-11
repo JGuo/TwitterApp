ef48d2230b76dc48217be2b45fad785dc592a3d6
Jisi Guo <jisi.guo@accenture.com>
2012-06-11 15:36:05 -0700

    finished user edit, update, index and destroy /w admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,8 +24,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   
   it { should be_valid }
+  it { should_not be_admin }
   
   describe "when name is not present" do
     before { @user.name = " " }
@@ -113,6 +115,11 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
 
+  describe "wehn admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+    
+    it { should be_admin }
+  end
   
 
 end
